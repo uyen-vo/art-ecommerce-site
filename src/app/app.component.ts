cc7283db05cc8e412f81fd114c4d467eb795c73a
Uyen Vo <uyenvo@tamu.edu>
2020-10-18 03:02:55 -0500

    flexbox ''fun''

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,14 +5,14 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   numCart = 0;
 
   ngOnInit(): void {
   }
 
   onActivate(event) {
-    window.scroll(0,0);
+    window.scroll(0, 0);
     this.numCart = Number(document.getElementsByClassName('snipcart-items-count')[0].textContent);
   }
 }
